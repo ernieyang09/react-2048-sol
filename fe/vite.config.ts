ae6d4819788fe73753855ea3f73dfd662ac97592
Ernie Yang <ernieyang09@hotmail.com>
2023-08-10 09:40:28 +0800

    fix: fix fe env

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,25 +1,32 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import linaria from '@linaria/vite'
 
 const PROJECT_NAME = 'react-2048-sol'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    linaria({
-      include: ['**/*.{ts,tsx}'],
-      babelOptions: {
-        presets: ['@babel/preset-typescript', '@babel/preset-react'],
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      linaria({
+        include: ['**/*.{ts,tsx}'],
+        babelOptions: {
+          presets: ['@babel/preset-typescript', '@babel/preset-react'],
+        },
+      }),
+    ],
+    base: `/${PROJECT_NAME}`,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-    }),
-  ],
-  base: `/${PROJECT_NAME}`,
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
     },
-  },
+    define: {
+      'process.env.CHAIN_ID': env.CHAIN_ID,
+    },
+  }
 })
